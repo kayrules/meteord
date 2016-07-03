1d71c86973fed9d72840c1dde7b6af194d581034
KayRules <kayrules@gmail.com>
2016-07-04 07:56:12 +0800

    added fixlibs repo

---
+++ fixlibs/scripts/run_app.sh
@@ -0,0 +1,55 @@
+set -e
+
+if [ -d /bundle ]; then
+  cd /bundle
+  tar xzf *.tar.gz
+  cd /bundle/bundle/programs/server/
+  npm i
+  cd /bundle/bundle/
+elif [[ $BUNDLE_URL ]]; then
+  cd /tmp
+  curl -L -o bundle.tar.gz $BUNDLE_URL
+  tar xzf bundle.tar.gz
+  cd /tmp/bundle/programs/server/
+  npm i
+  cd /tmp/bundle/
+elif [ -d /built_app ]; then
+  cd /built_app
+else
+  echo "=> You don't have any meteor app to run in this image."
+  exit 1
+fi
+
+if [[ $REBUILD_NPM_MODULES ]]; then
+  if [ -f /opt/meteord/rebuild_npm_modules.sh ]; then
+    cd programs/server
+    bash /opt/meteord/rebuild_npm_modules.sh
+    cd ../../
+  else
+    echo "=> Use meteorhacks/meteord:binbuild for binary bulding."
+    exit 1
+  fi
+fi
+
+if [[ $REBUILD_BCRYPT ]]; then
+  if [ -f /bundle/bundle/programs/server/npm/node_modules/meteor/npm-bcrypt ]; then
+    cd /bundle/bundle/programs/server/npm/node_modules/meteor/npm-bcrypt
+    npm i bcrypt
+    cd /bundle/bundle/
+  else
+    echo "=> bundle/programs/server/npm/node_modules/meteor/npm-bcrypt directory not found."
+    exit 1
+  fi
+fi
+
+# Set a delay to wait to start meteor container
+if [[ $DELAY ]]; then
+  echo "Delaying startup for $DELAY seconds"
+  sleep $DELAY
+fi
+
+# Honour already existing PORT setup
+export PORT=${PORT:-80}
+
+echo "=> Starting meteor app on port:$PORT"
+node main.js
